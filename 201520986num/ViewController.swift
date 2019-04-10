f45744b2ab6ae632f811564c6e433c44d5d444a6
dit06 <dit@imac-5.local>
2019-04-10 14:04:53 +0900

    Initial Commit

---
+++ 201520986num/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  201520986num
+//
+//  Created by dit06 on 2019. 4. 10..
+//  Copyright © 2019년 dit06. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
